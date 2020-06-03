e39ee9f6937e2679fa2f1b699b450c4a07ffd5fc
Olivia Cubela <oliviacubela@gmail.com>
2020-06-03 23:19:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,8 @@ def badge_maker(name)
 end
 badge_maker("Arel")
 
-def batch_badge_creator(name)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.map do |name|
+def batch_badge_creator(attendees)
+  attendees.map do |name| 
     "Hello, my name is #{name}."
 end
 end
